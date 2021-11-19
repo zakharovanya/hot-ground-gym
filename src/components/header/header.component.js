b630636b9705a00fe3a7e8e8588047a0dacfe7e4
Anna <zakharova.anya@list.ru>
2021-11-19 09:37:04 +0300

    fix menu

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,25 +4,23 @@ const modileMenu = document.querySelector('.menu-list');
 const burgerButton = document.querySelector('.burger-button');
 const addressInHeader = document.querySelector('.address-section');
 const rootBlock = document.querySelector('.root');
-const headerSection = document.querySelector('.header');
 let lastScrollTop = 0;
 
 window.onload = () => {
   modileMenu.classList.add('mobile-menu-hide');
-  addressInHeader.classList.toggle('mobile-address-show');
+  addressInHeader.classList.toggle('address-show');
   rootBlock.classList.toggle('root-hide-menu');
 };
 
 window.addEventListener('scroll', () => {
   const st = window.pageYOffset || document.documentElement.scrollTop;
   if (st > lastScrollTop) {
-    headerSection.classList.add('fixed-header');
-    addressInHeader.classList.remove('mobile-address-show');
-    addressInHeader.classList.add('mobile-address-hide');
+    addressInHeader.classList.remove('address-show');
+    addressInHeader.classList.add('address-hide');
     modileMenu.classList.remove('m-t-104-mobile');
   } else {
-    addressInHeader.classList.add('mobile-address-show');
-    addressInHeader.classList.remove('mobile-address-hide');
+    addressInHeader.classList.add('address-show');
+    addressInHeader.classList.remove('address-hide');
     modileMenu.classList.add('m-t-104-mobile');
   }
   lastScrollTop = st;
